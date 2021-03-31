ded505f418dbf496610c0dd8a62e4159d4ffd7a4
Elena Feoktistova <e.feoktistova@bars.group>
2021-03-31 23:26:13 +0300

    add spring boot and other dependencies

--- src/main/java/com/lonelydutchhound/Test.java
+++ src/main/java/com/lonelydutchhound/Test.java
@@ -1,12 +1,12 @@
 package com.lonelydutchhound;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Test {
-    public static final Logger logger = LoggerFactory.getLogger(Test.class);
+    static Logger logger = LoggerFactory.getLogger(Test.class);
     public static void main(String[] args) {
-        System.out.println("Test");
+        logger.info("Test");
+        logger.debug("Test debug");
     }
 }
